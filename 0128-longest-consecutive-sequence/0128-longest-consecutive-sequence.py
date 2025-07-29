626dfaee15de9696c13d7e4e698f0e63ef33f747
Jaipanapana <138653021+Jaipanapana@users.noreply.github.com>
2025-07-29 12:53:34 +0530

    Time: 39 ms (97.15%), Space: 34.2 MB (52.4%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -3,15 +3,12 @@ class Solution:
         if nums==[]:
             return 0
         nums=sorted(list(set(nums)))
-        print(nums)
         count=val=1
         for i in range(len(nums)-1):
-            print(nums[i]+1,nums[i+1])
             if nums[i]+1==nums[i+1]:
                 count+=1
             else:
                 if val<count:
-                    print(count)
                     val=count
                 count=1
         if val<count:
