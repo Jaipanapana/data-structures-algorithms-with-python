f9a3b65d7dd4003635bbb12786b8bd1bd1057065
Jaipanapana <138653021+Jaipanapana@users.noreply.github.com>
2025-07-30 12:17:27 +0530

    Time: 387 ms (97.48%), Space: 21.9 MB (8.3%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -3,6 +3,8 @@ class Solution:
         tripletset=set()
         nums.sort()
         for i in range(len(nums)):
+            if i>0 and nums[i]==nums[i-1]:
+                continue
             j=i+1
             k=len(nums)-1
             if nums[i]>0:
@@ -13,6 +15,12 @@ class Solution:
                 if sum==rem:
                     triplet=tuple(sorted([nums[i],nums[j],nums[k]]))
                     tripletset.add(triplet)
+                    while(j<k and nums[j]==nums[j+1]):
+                        j+=1
+                        continue
+                    while(j<k and nums[k]==nums[k-1]):
+                        k-=1
+                        continue
                     j+=1
                     k-=1
                 elif sum>rem:
