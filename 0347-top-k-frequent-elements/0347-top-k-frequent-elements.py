f819b7aa74a00ae63ddb59a500428405525359c6
Jaipanapana <138653021+Jaipanapana@users.noreply.github.com>
2025-09-08 11:40:12 +0530

    Time: 67 ms (5.04%), Space: 20.9 MB (99.28%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,28 +1,19 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         dic={}
-        for num in nums:
-            if num in dic:
-                dic[num]+=1
+        for val in nums:
+            if val not in dic:
+                dic[val]=1
             else:
-                dic[num]=1
-        check=0
-        lst=list()
-        for i in dic:
-            lst.append(dic[i])
+                dic[val]+=1
+        lst=list(dic.values())
         lst.sort(reverse=True)
-        val=0
-        result=list()
-        val=0
-        while(val<k):
-            for key in dic:
-                if lst[val]==dic[key]:
-                    result.append(key)
-                    del dic[key]
-                    break
-            val+=1
-        return result
-
-
-
-        
+        res=[]
+        for i in range(k):
+            val=lst[i]
+            d=dic.copy()
+            for j in d:
+                if dic[j]==val:
+                    res.append(j)
+                    del dic[j]
+        return res
