5d0a4b65d88479a4c1f41077d9570e4b6d731238
Jaipanapana <138653021+Jaipanapana@users.noreply.github.com>
2025-09-14 21:49:07 +0530

    Time: 1357 ms (5.1%), Space: 19.7 MB (98.58%) - LeetHub

--- 1006-vowel-spellchecker/1006-vowel-spellchecker.py
+++ 1006-vowel-spellchecker/1006-vowel-spellchecker.py
@@ -1,47 +1,37 @@
 class Solution:
     def spellchecker(self, wordlist: List[str], queries: List[str]) -> List[str]:
+        case_error={}
+        vowels_error={}
         res=[]
-        lower_case=[]
-        remove_vowels=[]
-        wordlist_updated=[]
-        for letter in wordlist:
-            word=letter.lower()
-            lower_case.append(word)
+        def conver_to_consonants(word):
             s=''
-            s1=''
-            for i in letter:
-                if i not in 'aeiouAEIOU':
-                    s+=i
-                    s1+=i
+            for i in word:
+                if i in 'aeiouAEIOU':
+                    s+='*'
                 else:
-                    s1+='@'
-            wordlist_updated.append(s1)
-            remove_vowels.append(s)
+                    s+=i
+            return s
+
+        for word in wordlist:
+            if word.lower() not in case_error:
+                case_error[word.lower()]=word
+            s=conver_to_consonants(word)
+            if s.lower() not in vowels_error:
+                vowels_error[s.lower()]=word
+        
         for word in queries:
-            flag1=0
-            flag2=0
-            flag3=0
-            new_word='' 
+            flag1=flag2=flag3=0
             if word in wordlist:
                 res.append(word)
+                flag1=1
+            elif word.lower() in case_error:
+                res.append(case_error[word.lower()])
                 flag2=1
-            elif word.lower() in lower_case:
-                for i in range(len(wordlist)):
-                    if word.lower()==lower_case[i]:
-                        res.append(wordlist[i])
-                        flag1=1
-                        break     
             elif flag1==0 and flag2==0:
-                for i in word:
-                    if i in 'aeiouAEIOU':
-                        new_word+='@'
-                    else:
-                        new_word+=i
-                for i in range(len(wordlist)):
-                    if new_word.lower()==wordlist_updated[i].lower():
-                        res.append(wordlist[i])
-                        flag3=1
-                        break
+                s=conver_to_consonants(word)
+                if s.lower() in vowels_error:
+                    res.append(vowels_error[s.lower()])
+                    flag3=1
             if flag1==0 and flag2==0 and flag3==0:
                 res.append("")
         return res
