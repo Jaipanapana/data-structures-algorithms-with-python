b19c0724ac77fc4cd6374f391364b361c6f0006e
Jaipanapana <138653021+Jaipanapana@users.noreply.github.com>
2025-09-22 21:42:55 +0530

    Time: 0 ms (100%), Space: 17.6 MB (77.56%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.py
+++ 0520-detect-capital/0520-detect-capital.py
@@ -1,24 +1,3 @@
 class Solution:
     def detectCapitalUse(self, word: str) -> bool:
-        c=lower=upper=f1=f2=0
-        for i in word:
-            if 'a'<=i<='z' :
-                if c==0:
-                    lower=1
-                    c+=1
-                f1=1
-            else:
-                if c==0:
-                    upper=1
-                    c+=1
-                else:
-                    f2=1
-        if upper==1 and f2==1 and f1==0:
-            return True
-        elif upper==1 and f2==0 and f1==1:
-            return True
-        elif lower==1 and f1==1 and f2==0:
-            return True
-        elif upper==1 and f1==0 and f2==0:
-            return True
-        return False 
+        return (word.isupper() or word.islower() or (word[0].isupper and word[1:]).islower())
