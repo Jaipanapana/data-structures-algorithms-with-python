7bc057f04f5bf1a8a1194ea73db2d915910a55fb
Jaipanapana <138653021+Jaipanapana@users.noreply.github.com>
2025-10-04 00:29:37 +0530

    Time: 2 ms (76.78%), Space: 18.2 MB (95.71%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,22 +1,17 @@
-from copy import deepcopy
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        dummy= deepcopy(matrix)
-        
+        row=[0]*len(matrix)
+        col=[0]*len(matrix[0])
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
                 if matrix[i][j]==0:
-                    k=0
-                    while k<len(matrix[0]):
-                        dummy[i][k]=0
-                        k+=1
-                    k=0 
-                    while k<len(matrix):
-                        dummy[k][j]=0 
-                        k+=1
-        print(dummy)                
-        matrix[:]=dummy
-
+                    row[i]=1
+                    col[j]=1
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if row[i]==1 or col[j]==1:
+                    matrix[i][j]=0
+        
